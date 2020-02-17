bb9700d265157ef97877dae71892f2092c2ca59c
quentinverlhac <quentin.verlhac@gmail.com>
2020-02-17 16:02:10 +0100

    :bento: update datasets config

--- main.py
+++ main.py
@@ -8,13 +8,3 @@ import audiosegment as extended_audiosegment
 from pydub import AudioSegment
 from tqdm import tqdm_notebook
 import pickle as pkl
-
-# Config variables
-
-# Set this to test code on limited sample number. Useful to test faster.
-IS_DEV_MODE = False
-DEV_MODE_SAMPLE_NUMBER = 10
-
-EMOTIFY_DATA_PATH = "/content/drive/My Drive/Deep Fried Learning/Datasets/emotifymusic"
-EMOTIFY_LABELS_PATH = "/content/drive/My Drive/Deep Fried Learning/Datasets/emotifymusic/data.csv"
-EMOTIFY_DUMP_PATH = "/content/drive/My Drive/Deep Fried Learning/Datasets/emotifymusic/data.pkl"
