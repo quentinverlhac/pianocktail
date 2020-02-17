19504fb7e39cbef24652d6b8853aa35bc7bd524f
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-02-17 15:59:51 +0100

    Add config file

---
+++ config.py
@@ -0,0 +1,9 @@
+# Config variables
+
+# Set this to test code on limited sample number. Useful to test faster.
+IS_DEV_MODE = False
+DEV_MODE_SAMPLE_NUMBER = 10
+
+EMOTIFY_DATA_PATH = "/content/drive/My Drive/Deep Fried Learning/Datasets/emotifymusic"
+EMOTIFY_LABELS_PATH = "/content/drive/My Drive/Deep Fried Learning/Datasets/emotifymusic/data.csv"
+EMOTIFY_DUMP_PATH = "/content/drive/My Drive/Deep Fried Learning/Datasets/emotifymusic/data.pkl"
