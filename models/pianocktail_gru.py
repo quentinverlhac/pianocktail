ff0af65e62ee35c132ed0d5611a59ee360260508
quentinverlhac <quentin.verlhac@gmail.com>
2020-02-24 18:51:53 +0100

    :construction: change GRU

--- models/pianocktail_gru.py
+++ models/pianocktail_gru.py
@@ -12,12 +12,15 @@ class PianocktailGRU(Model):
 
         self.input_layer = InputLayer((subspectrogram_points, mel_bins), name=f"{name}_input")
         
-        self.gru = GRU(subspectrogram_points, name=f"{name}_gru")
+        self.gru = GRU(100, name=f"{name}_gru")
 
-        self.dense = Dense(number_of_emotions, activation=tf.nn.sigmoid, name=f"{name}_output")
+        self.dense = Dense(50, activation=tf.nn.relu, name=f"{name}_dense")
+
+        self.output_layer = Dense(number_of_emotions, activation=tf.nn.sigmoid, name=f"{name}_output")
 
     def call(self, inputs, training=True):
         net = self.input_layer(inputs)
         net = self.gru(net, training=training)
         net = self.dense(net)
+        net = self.output_layer(net)
         return net
