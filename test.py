0b3ffcfa7e15391d8c5bd25ae620963a1a1e78a9
quentinverlhac <quentin.verlhac@gmail.com>
2020-02-27 16:20:47 +0100

    :construction: use new pipeline in test_model

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+import config
 from utils import display_and_reset_metrics
-from pipeline import *
+from pipeline import initialize_pipeline
+from models.basic_cnn import BasicCNN
+from models.pianocktail_gru import PianocktailGRU
 
-def test_model():
+def test_model(test_dataset, forward_pass, test_loss, test_accuracy):
     print(f"======================== evaluation on test data =========================")
     for iteration, (spectro, label) in enumerate(test_dataset):
         predictions, loss = forward_pass(spectro, label)
@@ -12,5 +15,15 @@ def test_model():
 
     display_and_reset_metrics(test_loss, test_accuracy, predictions, label, is_test=True)
 
+def initialize_and_test_model(model, batch_size, random_pick, learning_rate, should_restore_checkpoint = False):
+    forward_pass, _, _, test_dataset, _, _, test_loss, test_accuracy, _, _ = initialize_pipeline(model, batch_size, random_pick, learning_rate, should_restore_checkpoint = False)
+    test_model(test_dataset, forward_pass, test_loss, test_accuracy)
+
 if __name__ == '__main__':
-    test_model()
+    model = PianocktailGRU()
+    batch_size = config.BATCH_SIZE
+    random_pick = config.RANDOM_PICK
+    learning_rate = config.LEARNING_RATE
+    should_restore_checkpoint = config.RESTORE_CHECKPOINT
+    
+    initialize_and_test_model(model, batch_size, random_pick, learning_rate, should_restore_checkpoint = False)
