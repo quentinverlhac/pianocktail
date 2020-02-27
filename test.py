cc447f4bdfce5185912dd95e33c5067a7aed0dec
Antoine-marchais <antoine.marchais@student.ecp.fr>
2020-02-27 18:01:51 +0100

    fix test pipeline

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from utils import display_and_reset_metrics, load_dump, segment_spectrogram
+from utils import display_and_reset_metrics, load_dump, segment_spectrogram, load_model
 import tensorflow as tf
 import argparse
 
@@ -16,27 +16,32 @@ def test_model(model):
     print(f"======================== evaluation on test data =========================")
     for iteration in range(len(test_labels)):
         label = test_labels[iteration]
+        print(label)
         predictions = average_predictions(test_spectrograms[iteration], model)
-
+        print(predictions)
         # compute metrics
-        test_loss.update_state(label, predictions)
+        loss = tf.keras.metrics.binary_crossentropy(y_pred=predictions, y_true=label)
+        test_loss.update_state(loss)
         test_accuracy.update_state(label, predictions)
 
     display_and_reset_metrics(test_loss, test_accuracy, predictions, label, is_test=True)
 
 def average_predictions(full_spectrogram, model):
+    """
+    Average predictions of the model over all segments in the spectrogram
+    """
     segmented_spectro = segment_spectrogram(full_spectrogram, config.SUBSPECTROGRAM_DURATION_S, config.FFT_RATE)
-    mean_predictions = tf.metrics.Mean()
+    all_predictions = []
     for spectro in segmented_spectro:
         tensor_spectro = tf.convert_to_tensor(spectro)
         tensor_spectro = tf.transpose(tensor_spectro)
-        predictions = model.call(tensor_spectro)
-        mean_predictions.update_state(predictions)
-    return mean_predictions.result()
+        tensor_spectro = tf.reshape(tensor_spectro,[1,tensor_spectro.shape[0],tensor_spectro.shape[1]])
+        all_predictions.append(model.call(tensor_spectro))
+    return tf.add_n(all_predictions)/len(segmented_spectro)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("model_path",help="path of the model to evaluate")
     args = parser.parse_args()
-    model = tf.keras.models.load_model(args.model_path)
+    model = load_model(args.model_path)
     test_model(model)
