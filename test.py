1bc40f64918dae506216321488d66b5c13fdb1c2
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-02-27 18:10:18 +0100

    Prettify

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import argparse
 
 import config
 
+
 def test_model(model):
     # import data and labels
     test_spectrograms = load_dump(config.TEST_DATA_PATH)
@@ -14,10 +15,9 @@ def test_model(model):
     test_accuracy = tf.keras.metrics.BinaryAccuracy(name='test_accuracy')
 
     print(f"======================== evaluation on test data =========================")
-    for iteration in range(len(test_labels)):
-        label = test_labels[iteration]
+    for i, label in enumerate(test_labels):
         print(label)
-        predictions = average_predictions(test_spectrograms[iteration], model)
+        predictions = average_predictions(test_spectrograms[i], model)
         print(predictions)
         # compute metrics
         loss = tf.keras.metrics.binary_crossentropy(y_pred=predictions, y_true=label)
@@ -25,6 +25,7 @@ def test_model(model):
         test_accuracy.update_state(label, predictions)
 
     display_and_reset_metrics(test_loss, test_accuracy, predictions, label, is_test=True)
+
 
 def average_predictions(full_spectrogram, model):
     """
@@ -39,9 +40,10 @@ def average_predictions(full_spectrogram, model):
         all_predictions.append(model.call(tensor_spectro))
     return tf.add_n(all_predictions)/len(segmented_spectro)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("model_path",help="path of the model to evaluate")
+    parser.add_argument("model_path", help="path of the model to evaluate")
     args = parser.parse_args()
     model = load_model(args.model_path)
     test_model(model)
