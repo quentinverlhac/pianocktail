9caa7415fc87a8d5c717b2b8c7a363c3d969d0b7
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-02-29 17:07:55 +0100

    Create validation set in split.py

--- split.py
+++ split.py
@@ -16,20 +16,25 @@ index = np.array([[i+1] for i in range(len(all_spectro))])
 
 labels = utils.load_dump(config.EMOTIFY_LABELS_DUMP_PATH)
 
-# Train/test split 80/20, stratify on highest ranked emotion
-train_x_index, train_y, test_x_index, test_y = iterative_train_test_split(
+# Train/val+test split, use iterative stratification
+train_x_index, train_y, test_val_x_index, test_val_y = iterative_train_test_split(
     index, np.asarray(labels, dtype=np.int), test_size=(1-config.TRAIN_SIZE))
 
+# Val/test split, use iterative stratification
+val_x_index, val_y, test_x_index, test_y = iterative_train_test_split(
+    test_val_x_index, test_val_y, test_size=(1-config.VAL_SIZE/(1-config.TRAIN_SIZE)))
+
 train_x = [all_spectro[index[0]-1] for index in train_x_index]
+val_x = [all_spectro[index[0]-1] for index in val_x_index]
 test_x = [all_spectro[index[0]-1] for index in test_x_index]
 
 # Shuffle train data and labels
-temp = list(zip(train_x, train_y))
-random.shuffle(temp)
-train_x, train_y = zip(*temp)
+train_x, train_y = utils.shuffle_data_and_labels(train_x, train_y)
 
 # Dump split data
 utils.dump_elements(train_x, config.TRAIN_DATA_PATH)
 utils.dump_elements(train_y, config.TRAIN_LABELS_PATH)
+utils.dump_elements(val_x, config.VAL_DATA_PATH)
+utils.dump_elements(val_y, config.VAL_LABELS_PATH)
 utils.dump_elements(test_x, config.TEST_DATA_PATH)
 utils.dump_elements(test_y, config.TEST_LABELS_PATH)
